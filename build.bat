136bc1c1fc355bc57c9d86b8c49490689e9feb2c
Jacob <jaketehspartansalt@gmail.com>
2021-01-06 13:04:03 -0800

    now the Z80 is fully disassembled!

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+"tools/asm68k.exe" /o ae- /p "sound/z80/z80.asm", z80.unc, , z80.lst >z80.log
 "tools/asm68k.exe" /o ae- /p main.asm, out.md, , out.lst>out.log
 type out.log
 if not exist out.md pause & exit
