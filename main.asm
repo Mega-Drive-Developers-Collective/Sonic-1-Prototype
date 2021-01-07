44d2f850217844f9722b7191c128ee760a11a92c
Natsumi <retrogreensnake@gmail.com>
2021-01-07 07:25:08 +0200

    Delete extra empty lines

--- main.asm
+++ main.asm
@@ -25505,87 +25505,52 @@ sD2:		incbin "sound/sfx/D2.ssf"
 ; Segment type: Regular
 ; segment "RAM"
 RAM		section bss, org($FFFF0000), size($10000)
-
 Chunks:		ds.b $80					; note to self: sort this out later
 
 byte_FF0080:	ds.b $880
-
 byte_FF0900:	ds.b $720
-
 byte_FF1020:	ds.b $70E
-
 byte_FF172E:	ds.b $1C52
-
 byte_FF3380:	ds.b $C80
-
 byte_FF4000:	ds.b 8
-
 byte_FF4008:	ds.l 1
-
 byte_FF400C:	ds.b $22
-
 byte_FF402E:	ds.b $3D2
-
 byte_FF4400:	ds.b $3C00
-
 byte_FF8000:	ds.b $2400
-
 Layout:		ds.b $400
-
 byte_FFA800:	ds.b $200
-
 byte_FFAA00:	ds.b $100
-
 byte_FFAB00:	ds.b $100
-
 DisplayLists:	ds.b $400
-
 Blocks:		ds.b $1800
-
 SonicArtBuf:	ds.b $300
-
 SonicPosTable:	ds.b $100
-
 ScrollTable:	ds.b $400
-
 ObjectsList:	ds.b $40
-
 byte_FFD040:	ds.b $40
-
 byte_FFD080:	ds.b $40
-
 byte_FFD0C0:	ds.b $40
-
 byte_FFD100:	ds.b $40
-
 byte_FFD140:	ds.b $40
-
 byte_FFD180:	ds.b $40
-
 byte_FFD1C0:	ds.b $40
-
 byte_FFD200:	ds.b $40
-
 byte_FFD240:	ds.b $40
-
 byte_FFD280:	ds.b $40
-
 byte_FFD2C0:	ds.b $40
 		ds.b $40
 		ds.b $40
 		ds.b $40
 		ds.b $40
-
 byte_FFD400:	ds.b $40
 		ds.b $40
 		ds.b $40
 		ds.b $40
-
 byte_FFD500:	ds.b $40
 		ds.b $40
 		ds.b $40
 		ds.b $40
-
 byte_FFD600:	ds.b $40
 		ds.b $40
 		ds.b $40
@@ -25594,30 +25559,20 @@ byte_FFD600:	ds.b $40
 		ds.b $40
 		ds.b $40
 		ds.b $40
-
 LevelObjectsList:ds.b $1800
-
 SoundMemory:	ds.b $600
-
 GameMode:	ds.b 1
 		ds.b 1
-
 padHeldPlayer:	ds.b 1
-
 padPressPlayer:	ds.b 1
-
 padHeld1:	ds.b 1
-
 padPress1:	ds.b 1
-
 padPress2:	ds.b 1
-
 padHeld2:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 ModeReg2:	ds.w 1
 		ds.b 1
 		ds.b 1
@@ -25625,43 +25580,27 @@ ModeReg2:	ds.w 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 GlobalTimer:	ds.w 1
-
 dword_FFF616:	ds.l 1
-
 dword_FFF61A:	ds.l 1
-
 word_FFF61E:	ds.w 1
-
 word_FFF620:	ds.w 1
-
 word_FFF622:	ds.w 1
-
 word_FFF624:	ds.w 1
-
 word_FFF626:	ds.w 1
-
 byte_FFF628:	ds.b 1
-
 byte_FFF629:	ds.b 1
-
 VintRoutine:	ds.b 1
 		ds.b 1
-
 byte_FFF62C:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 word_FFF632:	ds.w 1
-
 word_FFF634:	ds.w 1
-
 RandomSeed:	ds.l 1
-
 PauseFlag:	ds.w 1
 		ds.b 1
 		ds.b 1
@@ -25671,15 +25610,12 @@ PauseFlag:	ds.w 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 word_FFF644:	ds.w 1
 		ds.b 1
 		ds.b 1
-
 word_FFF648:	ds.w 1
 		ds.b 1
 		ds.b 1
-
 word_FFF64C:	ds.w 1
 		ds.b 1
 		ds.b 1
@@ -25699,17 +25635,12 @@ word_FFF64C:	ds.w 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 word_FFF660:	ds.w 1
-
 word_FFF662:	ds.w 1
 		ds.b 1
 		ds.b 1
-
 word_FFF666:	ds.w 1
-
 LevSelOption:	ds.w 1
-
 LevSelSound:	ds.w 1
 		ds.b 1
 		ds.b 1
@@ -25731,109 +25662,83 @@ LevSelSound:	ds.w 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 plcList:	ds.b $60
-
 unk_FFF6E0:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF6E4:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF6E8:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF6EC:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF6F0:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF6F4:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF6F8:	ds.b 1
 		ds.b 1
-
 unk_FFF6FA:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 CameraX:	ds.l 1
-
 CameraY:	ds.l 1
-
 unk_FFF708:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF70C:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF710:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF714:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF718:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF71C:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF720:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF724:	ds.b 1
 		ds.b 1
-
 unk_FFF726:	ds.b 1
 		ds.b 1
-
 unk_FFF728:	ds.b 1
 		ds.b 1
-
 unk_FFF72A:	ds.b 1
 		ds.b 1
-
 unk_FFF72C:	ds.b 1
 		ds.b 1
-
 unk_FFF72E:	ds.b 1
 		ds.b 1
-
 unk_FFF730:	ds.b 1
 		ds.b 1
-
 unk_FFF732:	ds.b 1
 		ds.b 1
 		ds.b 1
@@ -25842,113 +25747,79 @@ unk_FFF732:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF73A:	ds.b 1
 		ds.b 1
-
 unk_FFF73C:	ds.b 1
 		ds.b 1
-
 unk_FFF73E:	ds.b 1
 		ds.b 1
-
 unk_FFF740:	ds.b 1
-
 unk_FFF741:	ds.b 1
-
 EventsRoutine:	ds.b 1
 		ds.b 1
-
 unk_FFF744:	ds.b 1
 		ds.b 1
-
 unk_FFF746:	ds.b 1
 		ds.b 1
-
 unk_FFF748:	ds.b 1
 		ds.b 1
-
 unk_FFF74A:	ds.b 1
-
 unk_FFF74B:	ds.b 1
-
 unk_FFF74C:	ds.b 1
-
 unk_FFF74D:	ds.b 1
-
 unk_FFF74E:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF754:	ds.b 1
 		ds.b 1
-
 unk_FFF756:	ds.b 1
 		ds.b 1
-
 unk_FFF758:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF75C:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF760:	ds.b 1
 		ds.b 1
-
 unk_FFF762:	ds.b 1
 		ds.b 1
-
 unk_FFF764:	ds.b 1
 		ds.b 1
-
 unk_FFF766:	ds.b 1
-
 unk_FFF767:	ds.b 1
-
 unk_FFF768:	ds.b 1
 		ds.b 1
-
 unk_FFF76A:	ds.b 1
 		ds.b 1
-
 unk_FFF76C:	ds.b 1
 		ds.b 1
-
 unk_FFF76E:	ds.b 1
 		ds.b 1
-
 unk_FFF770:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF774:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF778:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF77C:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF780:	ds.b 1
 		ds.b 1
-
 unk_FFF782:	ds.b 1
-
 unk_FFF783:	ds.b 1
 		ds.b 1
 		ds.b 1
@@ -25962,65 +25833,41 @@ unk_FFF783:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF790:	ds.b 1
 		ds.b 1
-
 unk_FFF792:	ds.b 1
 		ds.b 1
-
 unk_FFF794:	ds.b 1
 		ds.b 1
-
 Collision:	ds.l 1
-
 unk_FFF79A:	ds.b 1
 		ds.b 1
-
 unk_FFF79C:	ds.b 1
 		ds.b 1
-
 unk_FFF79E:	ds.b 1
 		ds.b 1
-
 unk_FFF7A0:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF7A4:	ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF7A7:	ds.b 1
-
 unk_FFF7A8:	ds.b 1
-
 unk_FFF7A9:	ds.b 1
-
 unk_FFF7AA:	ds.b 1
 		ds.b 1
-
 unk_FFF7AC:	ds.b 1
-
 unk_FFF7AD:	ds.b 1
-
 unk_FFF7AE:	ds.b 1
-
 unk_FFF7AF:	ds.b 1
-
 unk_FFF7B0:	ds.b 1
-
 unk_FFF7B1:	ds.b 1
-
 unk_FFF7B2:	ds.b 1
-
 unk_FFF7B3:	ds.b 1
-
 unk_FFF7B4:	ds.b 1
-
 unk_FFF7B5:	ds.b 1
-
 unk_FFF7B6:	ds.b 1
 		ds.b 1
 		ds.b 1
@@ -26063,7 +25910,6 @@ unk_FFF7B6:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF7E0:	ds.b 1
 		ds.b 1
 		ds.b 1
@@ -26079,9 +25925,7 @@ unk_FFF7E0:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFF7EF:	ds.b 1
-
 unk_FFF7F0:	ds.b 1
 		ds.b 1
 		ds.b 1
@@ -26098,40 +25942,27 @@ unk_FFF7F0:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 byte_FFF800:	ds.b $280
 		ds.b $80					; unused??
 
 Palette:	ds.b $80
-
 PaletteTarget:	ds.b $80
-
 byte_FFFC00:	ds.b $100
 		ds.b $100					; stack data!
 
 StackPointer:	ds.w 1
-
 LevelRestart:	ds.w 1
-
 LevelFrames:	ds.w 1
-
 byte_FFFE06:	ds.b 1
 		ds.b 1
-
 DebugRoutine:	ds.w 1
-
 byte_FFFE0A:	ds.b 1
-
 byte_FFFE0B:	ds.b 1
-
 unk_FFFE0C:	ds.b 1
 		ds.b 1
 		ds.b 1
-
 byte_FFFE0F:	ds.b 1
-
 level:		ds.w 1
-
 Lives:		ds.b 1
 		ds.b 1
 		ds.b 1
@@ -26141,31 +25972,19 @@ Lives:		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 byte_FFFE1B:	ds.b 1
-
 byte_FFFE1C:	ds.b 1
-
 ExtraLifeFlags:	ds.b 1
-
 byte_FFFE1E:	ds.b 1
-
 byte_FFFE1F:	ds.b 1
-
 Rings:		ds.w 1
-
 dword_FFFE22:	ds.l 1
-
 dword_FFFE26:	ds.l 1
 		ds.b 1
 		ds.b 1
-
 byte_FFFE2C:	ds.b 1
-
 byte_FFFE2D:	ds.b 1
-
 byte_FFFE2E:	ds.b 1
-
 byte_FFFE2F:	ds.b 1
 		ds.b 1
 		ds.b 1
@@ -26199,23 +26018,18 @@ byte_FFFE2F:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFFE50:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 word_FFFE54:	ds.w 1
-
 word_FFFE56:	ds.w 1
-
 byte_FFFE58:	ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 oscValues:	ds.b $42
 		ds.b 1
 		ds.b 1
@@ -26249,23 +26063,14 @@ oscValues:	ds.b $42
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 unk_FFFEC0:	ds.b 1
-
 unk_FFFEC1:	ds.b 1
-
 RingTimer:	ds.b 1
-
 RingFrame:	ds.b 1
-
 unk_FFFEC4:	ds.b 1
-
 unk_FFFEC5:	ds.b 1
-
 RingLossTimer:	ds.b 1
-
 RingLossFrame:	ds.b 1
-
 RingLossAccumulator:ds.w 1
 		ds.b 1
 		ds.b 1
@@ -26545,7 +26350,6 @@ RingLossAccumulator:ds.w 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 word_FFFFE0:	ds.w 1
 		ds.b 1
 		ds.b 1
@@ -26553,7 +26357,6 @@ word_FFFFE0:	ds.w 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 word_FFFFE8:	ds.w 1
 		ds.b 1
 		ds.b 1
@@ -26561,19 +26364,14 @@ word_FFFFE8:	ds.w 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 DemoMode:	ds.w 1
-
 DemoNum:	ds.w 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
 		ds.b 1
-
 ConsoleRegion:	ds.b 1
 		ds.b 1
-
 word_FFFFFA:	ds.w 1
-
 ChecksumStr:	ds.l 1
 ; end of 'RAM'
